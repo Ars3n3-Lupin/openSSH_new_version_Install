776541d341950f1acde32d1ae3f09f78dc1f2ecc
Ars3n3-Lupin <59227870+Ars3n3-Lupin@users.noreply.github.com>
2023-03-08 12:38:05 +0100

    Installation file

--- openSSH_opt_install.sh
+++ openSSH_opt_install.sh
@@ -26,10 +26,12 @@ $Blue"
 ********************************************************************************
 +                   This is an openSSH installation file                       +
 +                                                                              +
-+   0. Copy four files from SERVICES folder to /usr/lib/systemd/system/        +
++   0. Download SERVICES folder and change owners of the files insied          +
++      sudo chown root:root ./SERVICES/*                                       +
++      Copy four files from SERVICES folder to /usr/lib/systemd/system/        +
 +   1. First, a system update && upgrade                                       +
 +   2. Second, installation dependencies                                       +
-+   3. Third, compiling and install new version openSSH /opt                   +
++   3. Third, compiling and install new version openSSH into /opt folder       +
 +   4. Fourth, start ssh-latest.service                                        +
 +                                                                              +
 ********************************************************************************
